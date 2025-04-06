20f6c2d4462f9ee1ec4b4f24467681038faf66ad
angel-iscoding <angel.iscoding@gmail.com>
2025-04-06 11:26:41 -0500

    New menu desing

--- src/sections/dashboard/menuSection.jsx
+++ src/sections/dashboard/menuSection.jsx
@@ -9,24 +9,62 @@ const MenuSection = () => {
     const [nombre, useNombre] = useState('');
 
     return (
-        <>
+        <>  
+            <div className={style.container}>
                 <div className={style.logo}>
-                    <a href="#" onClick={() => navigate('/dashboard/')}><img src="../../../assets/images/Logo-removebg-preview.png" alt="Logo"/></a>
+                    <a href="#" onClick={() => navigate('/dashboard/')}><img src="/assets/icons/Vector.png" alt="Logo"/></a>
                 </div>
                 <div className={style.menu}>
                     <ul>
-                        <li className={style.link}><a href="#" onClick={() => navigate('/dashboard/')}><span className={`material-symbols-outlined ${style.icon}`}>home</span><p>Home</p></a></li>
-                        <li className={style.link}><a href="#" onClick={() => navigate('/dashboard/clases')}><span className={`material-symbols-outlined ${style.icon}`}>import_contacts</span><p>Clases</p></a></li>
-                        <li className={style.link}><a href="#" onClick={() => navigate('/dashboard/perfil')}><span className={`material-symbols-outlined ${style.icon}`}>account_circle</span><p>Perfil</p></a></li>
+                        <li className={style.link}>
+                            <a href="#" onClick={() => navigate('/dashboard/')}>
+                                <span class="material-symbols-outlined">home</span>
+                                <p className={style.p_link}>Home</p>
+                            </a>
+                        </li>
+                        <li className={style.link}>
+                            <a href="#" onClick={() => navigate('/dashboard/clases')}>
+                                <span class="material-symbols-outlined">calendar_today</span>
+                                <p className={style.p_link}>Schedule</p>
+                            </a>
+                        </li>
+                        <li className={style.link}>
+                            <a href="#" onClick={() => navigate('/dashboard/perfil')}>
+                                <span class="material-symbols-outlined">backpack</span>
+                                <p className={style.p_link}>Courses</p>
+                            </a>
+                        </li>
+                        <li className={style.link}>
+                            <a href="#" onClick={() => navigate('/dashboard/ajustes')}>
+                                <span class="material-symbols-outlined">videocam</span>
+                                <p className={style.p_link}>Videos</p>
+                            </a>
+                        </li>
+                        <li className={style.link}>
+                            <a href="#" onClick={() => navigate('/dashboard/ajustes')}>
+                                <span class="material-symbols-outlined">settings</span>
+                                <p className={style.p_link}>Ajustes</p>
+                            </a>
+                        </li>
                     </ul>
                 </div>
-                <div className={style.profile}>
-                    <a href="#" onClick={() => navigate('/dashboard/perfil')}>
-                        <img src={img} alt="Profile" className={style.foto}/>
-                    </a>
-                    <h3>{nombre}</h3>
+                <div className={style.options}>
+                    <ul>
+                        <li className={style.link}>
+                            <a href="#" onClick={() => navigate('/dashboard/clases')}>
+                            <span class="material-symbols-outlined">sms</span>
+                                <p className={style.p_link}>Support</p>
+                            </a>
+                        </li>
+                        <li className={style.link}>
+                            <a href="#" onClick={() => navigate('/dashboard/clases')}>
+                            <span class="material-symbols-outlined">exit_to_app</span>
+                                <p className={style.p_link}>Log out</p>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-            
+            </div>
         </>
     );
 }
