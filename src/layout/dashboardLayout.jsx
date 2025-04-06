27f11803b2c207d88b60f9f2b30c62875a68e8f7
angel-iscoding <angel.iscoding@gmail.com>
2025-04-06 11:27:23 -0500

    Preparing new dashboard

--- src/layout/dashboardLayout.jsx
+++ src/layout/dashboardLayout.jsx
@@ -3,15 +3,6 @@ import styles from "./dashboardLayout.module.css";
 import DashboardPage from "../pages/dashboardPage";
 
 const DashboardLayout = () => {
-
-    useEffect(() => {
-        document.body.style.background = 'linear-gradient(to right, #cbd5e1, #d6d3d1)';
-
-        return () => {
-            document.body.style.background = '';
-        };
-    }, [])
-
     return (
         <>
             <div className={styles.container}>
--- src/pages/dashboardPage.jsx
+++ src/pages/dashboardPage.jsx
@@ -8,17 +8,19 @@ import style from "./dashboardPage.module.css";
 
 const DashboardPage = () => {
     return (
-        <>  
-            <div className={style.left}>
+        <>
+            <div className={style.container_menu}>
                 <MenuSection/>
             </div>
-            <div className={style.right}>
+            <div className={style.container_dashboard}>
+            
+            </div>
+                {/* <MenuSection/>
                 <Routes>
                     <Route path="/" element={<HomeSection/>}/>
                     <Route path="/clases" element={<ClassesSection/>}/>
                     <Route path="/perfil" element={<ProfileSection/>}/>
-                </Routes>
-            </div>
+                </Routes> */}
         </>
     );
 }
