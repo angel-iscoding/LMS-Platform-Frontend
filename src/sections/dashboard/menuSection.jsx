37796b053791a2f21b38180d47324cf0ad1bc27f
angel-iscoding <angel.iscoding@gmail.com>
2025-04-11 22:04:43 -0500

    Size desing and width of Icons changed

--- src/sections/dashboard/menuSection.jsx
+++ src/sections/dashboard/menuSection.jsx
@@ -18,31 +18,31 @@ const MenuSection = () => {
                     <ul>
                         <li className={style.link}>
                             <a href="#" onClick={() => navigate('/dashboard/')}>
-                                <img src="/assets/icons/Figma Import/Home.png" width={27} alt="" />
+                                <img src="/assets/icons/Figma Import/Home.png" width={22} alt="" />
                                 <p className={style.p_link}>Home</p>
                             </a>
                         </li>
                         <li className={style.link}>
                             <a href="#" onClick={() => navigate('/dashboard/clases')}>
-                                <img src="/assets/icons/Figma Import/Calendar.png" width={27} alt="" />
+                                <img src="/assets/icons/Figma Import/Calendar.png" width={22} alt="" />
                                 <p className={style.p_link}>Schedule</p>
                             </a>
                         </li>
                         <li className={style.link}>
                             <a href="#" onClick={() => navigate('/dashboard/perfil')}>
-                                <img src="/assets/icons/Figma Import/Bag.png" width={27} alt="" />
+                                <img src="/assets/icons/Figma Import/Bag.png" width={22} alt="" />
                                 <p className={style.p_link}>Courses</p>
                             </a>
                         </li>
                         <li className={style.link}>
                             <a href="#" onClick={() => navigate('/dashboard/ajustes')}>
-                                <img src="/assets/icons/Figma Import/Video.png" width={27} alt="" />
+                                <img src="/assets/icons/Figma Import/Video.png" width={22} alt="" />
                                 <p className={style.p_link}>Videos</p>
                             </a>
                         </li>
                         <li className={style.link}>
                             <a href="#" onClick={() => navigate('/dashboard/ajustes')}>
-                                <img src="/assets/icons/Figma Import/Ajustes.png" width={27} alt="" />
+                                <img src="/assets/icons/Figma Import/Ajustes.png" width={22} alt="" />
                                 <p className={style.p_link}>Ajustes</p>
                             </a>
                         </li>
@@ -52,13 +52,13 @@ const MenuSection = () => {
                     <ul>
                         <li className={style.link}>
                             <a href="#" onClick={() => navigate('/dashboard/clases')}>
-                                <img src="/assets/icons/Figma Import/Chat.png" width={27} alt="" />
+                                <img src="/assets/icons/Figma Import/Chat.png" width={22} alt="" />
                                 <p className={style.p_link}>Support</p>
                             </a>
                         </li>
                         <li className={style.link}>
                             <a href="#" onClick={() => navigate('/dashboard/clases')}>
-                                <img src="/assets/icons/Figma Import/Sing out.png" width={27} alt="" />
+                                <img src="/assets/icons/Figma Import/Sing out.png" width={22} alt="" />
                                 <p className={style.p_link}>Log out</p>
                             </a>
                         </li>
